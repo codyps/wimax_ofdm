3f53ad7008dd208522eb540ab40e76337dc168f2
Cody Schafer <cpschafer@gmail.com>
2011-04-06 21:28:46 -0400

    cp: header.

---
+++ cp.v
@@ -0,0 +1,11 @@
+module cp(
+	input reset, clk,
+	input [1:0] param_G,
+	input cp_in_bits,
+	input cp_in_valid,
+	output cp_out_bits,
+	output cp_out_valid
+	/* FIXME: these widths are probably wrong. */
+);
+
+endmodule

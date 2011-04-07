f4a8bdee08af4069b34a936cec428e697d602762
Cody Schafer <cpschafer@gmail.com>
2011-04-06 20:50:30 -0400

    burst_handler: header.

---
+++ burst_handler.v
@@ -0,0 +1,15 @@
+module burst_handler(
+	input reset, clk,
+	input [3:0] iuc, bsid, frame_num,
+	input burst_in_bits, burst_in_valid,
+	output [14:0] rand_iv,
+	output rand_reload,
+	output burst_out_bits,
+	output burst_out_valid,
+	input [5:0] subchan_data_in,
+	output [5:0] subchan_data_out,
+	output [3:0] subchan_ct,
+	input [3:0] rate_id_in,
+	output [3:0] rate_id_out);
+
+endmodule

4c25b121d36f4ce33c2c5d2ce97be165f5dab916
Cody Schafer <cpschafer@gmail.com>
2011-04-06 20:53:40 -0400

    ec: header.

---
+++ error_correction.v
@@ -0,0 +1,11 @@
+module error_correction(
+	input reset, clk
+	input in_bits,
+	input in_valid,
+	output out_bits,
+	output out_valid,
+	input [3:0] rate_id
+	/* FIXME: I think this need more inputs */
+);
+
+endmodule
